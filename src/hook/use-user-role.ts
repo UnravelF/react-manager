16d6089828ca48d8b4e2f3dc1c5f5e7c4703c495
huanggaofeng <hgfhgf1225@163.com>
2024-11-14 01:39:41 +0800

    feat:添加面板页面及 echarts图表搭建

--- src/hook/use-user-role.ts
+++ src/hook/use-user-role.ts
@@ -1,11 +1,11 @@
-import { RoleEnum, RoleNameEnum } from "@/types/common";
+import { RoleEnum, RoleNameEnum } from '@/types/common';
 
 const useUserRole = (roleId: string | string[]) => {
   const userRole = roleId === RoleEnum.ADMIN ? RoleEnum.ADMIN : RoleEnum.USER;
   const isAdmin = userRole === RoleEnum.ADMIN;
-  const roleName = isAdmin ? RoleNameEnum["admin"] : RoleNameEnum["user"];
+  const roleName = isAdmin ? RoleNameEnum['admin'] : RoleNameEnum['user'];
 
-  return { isAdmin, roleName }
-}
+  return { isAdmin, roleName };
+};
 
-export {useUserRole};
+export { useUserRole };
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { memo } from 'react';
-import { Descriptions, DescriptionsProps } from 'antd';
+import React, { memo, useEffect } from 'react';
+import { Button, Card, Descriptions, DescriptionsProps } from 'antd';
+import * as echarts from 'echarts';
 
 import styles from './index.module.less';
 import store from '@/store';
@@ -38,6 +39,58 @@ const Dashboard: React.FC = memo(() => {
     }
   ];
 
+  useEffect(() => {
+    const lineChartDom = document.getElementById('lineChart');
+    const lineChartInstance = echarts.init(lineChartDom as HTMLElement);
+    lineChartInstance.setOption({
+      title: {
+        text: '月发表文章数及访问量'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['月文章数', '月访问量']
+      },
+      grid: {
+        left: 50,
+        right: 50,
+        bottom: 50
+      },
+      xAxis: {
+        data: [
+          '1月',
+          '2月',
+          '3月',
+          '4月',
+          '5月',
+          '6月',
+          '7月',
+          '8月',
+          '9月',
+          '10月',
+          '11月',
+          '12月'
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '月文章数',
+          type: 'line',
+          data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+        },
+        {
+          name: '月访问量',
+          type: 'line',
+          data: [100, 220, 330, 440, 550, 660, 770, 880, 990, 150, 350, 120]
+        }
+      ]
+    });
+  }, []);
+
   return (
     <div className={styles.dashboard}>
       <div className={styles.userInfo}>
@@ -73,6 +126,25 @@ const Dashboard: React.FC = memo(() => {
           <div className='title'>Antd</div>
           <div className={styles.data}>Ant Design of React</div>
         </div>
+      </div>
+      <div className={styles.chart}>
+        <Card
+          title='订单和流水走势图'
+          extra={<Button type='primary'>刷新</Button>}
+        >
+          <div id='lineChart' className={styles.itemLine}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='司机分布' extra={<Button type='primary'>刷新</Button>}>
+          <div id='pieChartCity' className={styles.itemLine}></div>
+          <div id='pieChartAge' className={styles.itemLine}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='模型诊断' extra={<Button type='primary'>刷新</Button>}>
+          <div id='radarChart' className={styles.itemLine}></div>
+        </Card>
       </div>
     </div>
   );
